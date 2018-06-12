0ab2eaea49354fbaaa06402cb6eb3a3f06a00f33
Shahaji Deshmukh <shahaji31.deshmukh@gmail.com>
2018-06-12 18:04:42 +0530

    Minified

--- src/routes/Expert/application/DocumentPreview.js
+++ src/routes/Expert/application/DocumentPreview.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import {ButtonDropdown, DropdownToggle, DropdownItem, DropdownMenu} from 'reactstrap';
+// import {ButtonDropdown, DropdownToggle, DropdownItem, DropdownMenu} from 'reactstrap';
 
 import { uploadFile, changeDocumentCategory, deleteDocument, fileTypeRejected } from '../../../actions/expert'
 
