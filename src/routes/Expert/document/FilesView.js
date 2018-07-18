46483413662c8e00c186a88f11594d45f7af6ce3
Shahaji Deshmukh <shahaji31.deshmukh@gmail.com>
2018-07-18 18:26:16 +0530

    image aspect possible fix

--- src/routes/Expert/document/FilesView.js
+++ src/routes/Expert/document/FilesView.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 
 import { deleteFile } from '../../../actions/expert'
 
+import './Document.scss'
 const imageTypes = [
     'jpeg',
     'jpg',
@@ -28,12 +29,12 @@ class FilesView extends React.Component {
                 {
                     // files.map(file =>
                         files ?
-                        <div  id='pdf' key={files._id} ref={node => {pdff = node}}>
+                        <div key={files._id} ref={node => {pdff = node}}>
                             {
                                 (files.uploadName.split('.').pop()).toLowerCase() === 'pdf' ?
                                 <embed style={{height:'-webkit-fill-available', width:'-webkit-fill-available'}} type='application/pdf' src={'/api/expert/files/'+files._id} alt='pdf'/> :
                                 imageTypes.indexOf((files.uploadName.split('.').pop()).toLowerCase()) !== -1 ?
-                                <img style={{height:'75vh', width:'48vw', position:'relative', left:'-1%'}} src={'/api/expert/files/'+files._id} /> :
+                                <span><img style={{maxHeight:'75vh', maxWidth:'48vw', position:'relative', left:'-1%'}} src={'/api/expert/files/'+files._id} /></span> :
                                 //<div><button onClick={() => { this.setState({ visible: !this.state.visible }); } }>show</button><Viewer visible={this.state.visible} ops-app/images={'/api/expert/files/'+this.props.files._id} zoomable={true} onClose={() => {this.setState({visible : ! this.state.visible})}}/> </div>:
                                 <a href={'/api/expert/files/'+files._id}>{files.uploadName}</a>
                             }
